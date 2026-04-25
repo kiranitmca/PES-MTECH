68fd61dc9f6a2a7a46c6027959fbe2038a581152
kiranit.mca <kiranit.mca@gmail.com>
2026-04-25 11:53:52 +0530

    pyton introduction

--- Python/introduction.py
+++ Python/introduction.py
@@ -20,10 +20,24 @@
 #     print('Sensor is on')
 #     print('Sensor is working fine')
 
-v1= 10
-print(type(v1))  # shows the type of variable v1
-v2 = 10.5
-v2= 'hey'
-print(type(v2))  # shows the type of variable v2
-print(type(v2))  # shows the type of variable v2 
-#python dynamically types lang and in python, there is no contract between  variable and  object value.
+# v1= 10
+# print(type(v1))  # shows the type of variable v1
+# v2 = 10.5
+# v2= 'hey'
+# print(type(v2))  # shows the type of variable v2
+# print(type(v2))  # shows the type of variable v2 
+# #python dynamically types lang and in python, there is no contract between  variable and  object value.
+
+# v3 = 2500
+# print(id(v3))  # shows memory address of the varaible 
+# v4= 2500
+# print(id(v4))   # value  -5 to 256 are created during the boot time and stored in the memory and they are shared among all the variables. so, v3 and v4 are pointing to the same memory address. but for value 258, it is not created during the boot time and it is created at runtime, so v3 and v4 are pointing to different memory address.
+
+# delete variable 
+
+#del v4
+#print(v4)
+# Keyboard inputcls
+
+# sub = input('Enter the subject name: ')
+# print('The subject name is: ' + sub)
