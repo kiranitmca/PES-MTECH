511516a8184aa5065c36bf7e930f86409dc59bdc
kiranit.mca <kiranit.mca@gmail.com>
2026-04-25 10:51:23 +0530

    pyton introduction

---
+++ Python/introduction.py
@@ -0,0 +1,29 @@
+# print('NSE')
+# print('BSE # Mumbai')
+
+# var1 = 'nse and bse is in ' \
+# 'mumbai'\
+# ' and ' \
+# 'delhi'
+# print(var1)
+
+# import keyword
+# print(keyword.kwlist)
+# print(dir(keyword))
+
+# sensor=0
+
+# if sensor == 1:
+#     print('Sensor is off')
+#     print('Please turn on the sensor')
+# else:
+#     print('Sensor is on')
+#     print('Sensor is working fine')
+
+v1= 10
+print(type(v1))  # shows the type of variable v1
+v2 = 10.5
+v2= 'hey'
+print(type(v2))  # shows the type of variable v2
+print(type(v2))  # shows the type of variable v2 
+#python dynamically types lang and in python, there is no contract between  variable and  object value.
