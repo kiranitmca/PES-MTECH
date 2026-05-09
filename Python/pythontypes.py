aa4369c3ab1804e608614ed9577f064fc5860381
kiranit.mca <kiranit.mca@gmail.com>
2026-05-09 12:40:01 +0530

    pyton introduction

--- Python/pythontypes.py
+++ Python/pythontypes.py
@@ -102,18 +102,114 @@ def objcheck(obj):
 
 # Dictionary is a collection of key-value pairs in python
 
-md1 = {'Bank':'SBI','Branch':'MGR','country':'India'}
+md1 = {'Bank':'SBI','Branch':'Whitefield','country':'India'}
 
 # print(type(md1))
 # print(md1['Bank'])
 # md1['Bank']='HDFC'
 # print(md1)
 # print(md1.get('Branch'))
-k=md1.keys()
-print(k)
+# k=md1.keys()
+# print(k)
 
-klist= list(md1.keys())
-print(klist)
+# klist= list(md1.keys())
+# print(klist)
 
-ilist= list(md1.items())
-print(ilist)
+# ilist= list(md1.items())
+# print(ilist)
+
+# Get Method to access value of a key in dictionary
+# print(md1.get('Bank','Unknown Key'))
+
+# r= md1.pop('State','Unknown Key')
+# print(r)
+
+# md1.setdefault('State','Maharashtra')
+# print(md1)
+
+# keys= ['Bank','Branch','country','City']
+# for k in keys:
+#     print(f"{k} :  {md1.get(k,'Unknown Key')}")
+
+
+md2 ={'Bank':'HDFC','Branch':'MGR','country':'India'}
+
+md1.update(md2)
+#print(md1)
+
+klist= ['Bank','Branch','country','City']
+vlist= ['SBI','Whitefield','India','Bangalore']
+# z=zip(klist,vlist) #  zip o/p is an iterator .one time consumable container
+# #print(list(z))
+
+# md3=dict(z)
+
+# print(md3)
+
+# for k,v in zip(klist,vlist):
+#     print(f"{k} : {v}")
+
+
+# slist=['S1','S2','S3']
+# m1list=[70,65,74]
+# m2list=[80,75,85]
+
+# mlist=dict(zip(slist,zip(m1list,m2list)))
+# print(mlist)
+
+
+# Sets are unordered collection of unique elements in python.
+
+# s1={10,20,30,40,50,20,30,60,70,80}
+# print(sorted(set(s1)))
+
+# # m5=set('Helloworld')
+# # print(m5)
+
+#ms1={10,20,30,40,50,80}
+#ms2={30,40,50,60,70,10,20}
+# ms3=ms1.union(ms2)
+# ms4=ms1.intersection(ms2)
+#ms5=ms1.difference(ms2)
+# ms6=ms1.symmetric_difference(ms2)
+# print(ms3)
+# print(ms2)
+# print(ms3)
+# print(ms4)
+#print(ms5)
+# print(ms6)
+
+# Decision Making
+
+# temp = int(input("Enter the temperature in Celsius: "))
+# if temp < 60:
+#     print("It's a Cold day.")
+# elif temp >=60 and temp <=99 :
+#     print("It's a pleasant day.")
+# elif temp > 99:
+#     print("It's a Hot day.")
+
+# temp1=98
+# r = 'Hot' if temp1 > 90 else"cold" if temp1 < 60 else 'Normal'
+# print(r)
+# import sys
+# r = range(10,20)
+# vlist= list(r)
+# #print(vlist)
+# print(sys.getsizeof(r))
+# print(sys.getsizeof(vlist))
+
+# Loops / Iterators
+
+# lcount1=10
+# lcount2=20
+# while lcount1 < lcount2:
+#     print(lcount1, end = ' ')
+#     lcount1+=2
+# print("Bye")
+
+
+l = range(10)
+for i in set(l):
+    if i%2==0:
+        print(f"Even Number: {i} ")
